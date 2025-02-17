b27e0c2a87d548bd53a2a113ebf4c791eca93246
smtilson <seantilson@gmail.com>
2025-02-17 16:56:01 +0100

    add serializer and some of the api boilerplate structure. add to readme.

--- test_api/stores/models.py
+++ test_api/stores/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
+
 # Create your models here.
+class Store(models.Model):
+    name = models.CharField(max_length=255)
+    owner = models.CharField(max_length=255)
+    managers = models.ManyToManyField("users.CustomUser", related_name="managed_stores")
+    address = models.CharField(max_length=255)
+    city = models.CharField(max_length=255)
+    state = models.CharField(max_length=2)
+    zip_code = models.CharField(max_length=5)
+    opening_time = models.TimeField()
+    closing_time = models.TimeField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
--- test_api/test_api/settings.py
+++ test_api/test_api/settings.py
@@ -103,6 +103,11 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+# REST framework settings
+REST_FRAMEWORK = {
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 3,
+}
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
