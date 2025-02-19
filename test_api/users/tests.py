bc6240ce96b8f7c8c6dec9524416412be3552a40
smtilson <seantilson@gmail.com>
2025-02-19 20:36:16 +0100

    add model tests

--- test_api/users/tests.py
+++ test_api/users/tests.py
@@ -1,3 +1,36 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+
+# Serializer Tests
+# These should test each serializer
+# testing serialization and deserialization
+# see that password is hashed/handled correctly
+# test CRUD operations
+# Test Signup serializer
+## new user can be created
+## that a token is created and returned
+# Test Login serializer
+## user can login
+## token is returned
+## user can't login with wrong password
+## user can't login with wrong email
+## bad credentials give a response
+
+# User Views Tests
+
+## Test CustomUserViewSet
+### Test that a user can be created
+### Test that a user can be updated
+### Test that a user can be deleted
+### Test that a user can be retrieved
+### Test that a user can be listed
+## SignupView
+### Test that a user can be created
+### Test that a token is created and returned
+### test edge cases
+## LoginView:
+### Test login with valid credentials and ensure a token is returned.
+### Test login with invalid credentials (e.g., wrong password or non-existent email).
+### Test edge cases like empty or malformed requests.
---
+++ test_api/users/tests/test_models.py
@@ -0,0 +1,65 @@
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+from rest_framework.authtoken.models import Token
+from django.db import IntegrityError
+
+
+# Model Tests
+# these chould test creation of users and token association at creation.
+
+User = get_user_model()
+
+
+class CustomUserModelTest(TestCase):
+    def setUp(self):
+        self.user_data = {
+            "email": "test@example.com",
+            # is this password sufficient?
+            "password": "secure_PASSWORD_123",
+            "first_name": "John",
+            "last_name": "Doe",
+        }
+
+    def test_create_user(self):
+        user = User.objects.create_user(**self.user_data)
+        # correct data
+        self.assertEqual(user.email, self.user_data["email"])
+        self.assertTrue(user.check_password(self.user_data["password"]))
+        self.assertEqual(user.first_name, self.user_data["first_name"])
+        self.assertEqual(user.last_name, self.user_data["last_name"])
+
+        # token associated with the user
+        self.assertIsNotNone(user.auth_token)
+        # does the auth token have this?
+        self.assertEqual(user.auth_token.user, user)
+
+        # user properties
+        self.assertFalse(user.is_superuser)
+        self.assertFalse(user.is_staff)
+        self.assertTrue(user.is_active)
+        self.assertFalse(user.is_owner)
+        self.assertFalse(user.is_manager)
+
+    def test_user_str(self):
+        user = User.objects.create_user(**self.user_data)
+        self.assertEqual(user.name, f"{user.first_name} {user.last_name}")
+        self.assertEqual(str(user), user.name + " (id: " + str(user.id) + ")")
+
+    def test_create_superuser(self):
+        superuser = User.objects.create_superuser(**self.user_data)
+        self.assertTrue(superuser.is_superuser)
+        self.assertTrue(superuser.is_staff)
+        self.assertTrue(superuser.is_active)
+
+    def test_token_on_creation(self):
+        user = User.objects.create_user(**self.user_data)
+        token = Token.objects.get(user=user)
+        self.assertEqual(token.user, user)
+        self.assertEqual(token.key, user.auth_token.key)
+
+    def test_email_uniqueness(self):
+        dummy_data = {"email": "test@example.com", "password": "testpassword"}
+        User.objects.create_user(**dummy_data)
+        dummy_data["password"] += "1234"
+        with self.assertRaises(IntegrityError):
+            User.objects.create_user(**dummy_data)
